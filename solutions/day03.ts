52ce524245a5191c1c075bb2b6b26f0b06cce0d4
Domagoj Marusic <bradazg@gmail.com>
2024-05-21 22:35:44 +0200

    day3 part2 solution

--- solutions/day03.ts
+++ solutions/day03.ts
@@ -13,6 +13,8 @@ function parseInput(path: string): Data[] {
     .map((line) => line.split(""));
 }
 
+// Part 1
+
 function checkAdjacentForSymbols(grid: Data[], i: number, j: number): boolean {
   const symbolRegex = /[^\w.]/;
 
@@ -67,3 +69,123 @@ const part1 = (data: Data[]): number => {
 };
 
 console.log(part1(parseInput(input)));
+
+// Part 2
+
+function checkAdjacentForSymbol(
+  grid: Data[],
+  i: number,
+  j: number,
+  starCoordArr: number[]
+): boolean {
+  // Check all 8 surrounding cells
+  for (let x = -1; x <= 1; x++) {
+    for (let y = -1; y <= 1; y++) {
+      if (x === 0 && y === 0) continue; // Skip the cell itself
+
+      const checkI = i + x;
+      const checkJ = j + y;
+
+      // Boundary checks
+      if (
+        checkI >= 0 &&
+        checkI < grid.length &&
+        checkJ >= 0 &&
+        checkJ < grid[checkI].length
+      ) {
+        if (starCoordArr[0] === checkI && starCoordArr[1] === checkJ) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+function checkAdjacentForTwoNumbers(
+  grid: Data[],
+  i: number,
+  j: number
+): number[] {
+  let coordArr: number[] = [];
+  let adjString = "";
+
+  // Check all 8 surrounding cells
+  for (let x = -1; x <= 1; x++) {
+    adjString += ".";
+    for (let y = -1; y <= 1; y++) {
+      const checkI = i + x;
+      const checkJ = j + y;
+
+      // Boundary checks
+      if (
+        checkI >= 0 &&
+        checkI < grid.length &&
+        checkJ >= 0 &&
+        checkJ < grid[checkI].length
+      ) {
+        adjString += grid[checkI][checkJ];
+      }
+      if (coordArr.length === 0) {
+        coordArr.push(i);
+        coordArr.push(j);
+      }
+    }
+  }
+
+  return adjString.match(/\d+/g).map(Number).length === 2 ? coordArr : [];
+}
+
+const part2 = (data: Data[]): number => {
+  const valueArr: number[] = [];
+  const starCoordArr: number[][] = [];
+  let value = "";
+  let symbolArr: boolean[] = [];
+  const gearsArr: number[] = [];
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      if (data[i][j] === "*") {
+        const [a, b] = checkAdjacentForTwoNumbers(data, i, j);
+        if (a && b) starCoordArr.push([a, b]);
+      }
+    }
+  }
+
+  for (let z = 0; z < starCoordArr.length; z++) {
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].length; j++) {
+        if (/\d/.test(data[i][j])) {
+          value += data[i][j];
+          // Check adjacent cells for symbol
+          const isAdjacentToSymbol = checkAdjacentForSymbol(
+            data,
+            i,
+            j,
+            starCoordArr[z]
+          );
+          if (isAdjacentToSymbol) {
+            symbolArr.push(true);
+          } else {
+            symbolArr.push(false);
+          }
+        } else {
+          if (value && symbolArr.some((val) => val === true)) {
+            valueArr.push(Number(value));
+          }
+          value = "";
+          symbolArr = [];
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i <= valueArr.length - 2; i += 2) {
+    gearsArr.push(valueArr[i] * valueArr[i + 1]);
+  }
+
+  return gearsArr.reduce((a, b) => a + b, 0);
+};
+
+console.log(part2(parseInput(input)));
